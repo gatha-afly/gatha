18d7c79e16668eada5c84a4a6745a223cc4d1047
besincielement <yesim.demir@dci.education>
2023-11-07 14:47:22 +0100

    groupController.js created

--- server/models/Group.js
+++ server/models/Group.js
@@ -1,5 +1,17 @@
 import { Schema, model } from "mongoose";
 
 const groupSchema = new Schema({
-  groupName: {},
+  groupName: { type: String, required: true },
+  groupDescription: { type: String },
+  groupAvatar: {
+    imgName: { type: String },
+    imgPath: { type: String },
+    imgType: { type: String },
+    imgSize: { type: Number },
+  },
+  groupMembers: [{ type: Schema.Types.ObjectId, ref: "User" }],
+  groupAdmin: { type: Schema.Types.ObjectId, ref: "User" },
+  groupMessages: [{ type: Schema.Types.ObjectId, ref: "Message" }],
 });
+const Group = model("Group", groupSchema);
+export default Group;
--- server/models/Message.js
+++ server/models/Message.js
@@ -1,6 +1,27 @@
 import { Schema, model } from "mongoose";
 
-const messageSchema = new Schema({});
+const messageSchema = new Schema(
+  {
+    author: { type: Schema.Types.ObjectId, ref: "User" },
+    message: {
+      text: { type: String, required: true },
+      img: {
+        imgName: { type: String },
+        imgPath: { type: String },
+        imgType: { type: String },
+        imgSize: { type: Number },
+      },
+      gif: {
+        gifName: { type: String },
+        gifPath: { type: String },
+        gifType: { type: String },
+        gifSize: { type: Number },
+      },
+    },
+    group: { type: Schema.Types.ObjectId, ref: "Group" },
+  },
+  { timestamps: true }
+);
 
 const Message = model("Message", messageSchema);
 export default Message;
