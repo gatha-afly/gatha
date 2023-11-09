b0d830ad2e9c384dc1a2f0bcd2513c556216df46
Abdulwase Naeemi <abdulwasenaeemi7@gmail.com>
2023-11-09 21:15:07 +0100

    the groupname has been validated

--- server/models/Group.js
+++ server/models/Group.js
@@ -1,7 +1,7 @@
 import { Schema, model } from "mongoose";
 
 const groupSchema = new Schema({
-  name: { type: String, required: true },
+  name: { type: String, required: true, unique: true },
   description: { type: String },
   avatar: {
     imgName: { type: String },
--- server/routes/groupRoutes.js
+++ server/routes/groupRoutes.js
@@ -1,11 +1,18 @@
 import express from "express";
 import { createGroup } from "../controllers/groupControllers.js";
 import { authorizeUser } from "../middleware/userAuthorization.js";
+import { validator } from "../middleware/validator.js";
+import { validateGroupRules } from "../middleware/groupSanitizer.js";
 
 const router = express.Router();
 
 // Protected endpoint
 router.use(authorizeUser);
-router.post("/create-group/:userId", createGroup);
+router.post(
+  "/create-group/:userId",
+  validateGroupRules,
+  validator,
+  createGroup
+);
 
 export default router;
