ff638b18f510655196cc8a6475b242bd8ed0f104
Abdulwase Naeemi <abdulwasenaeemi7@gmail.com>
2023-11-14 09:29:45 +0100

    the conflict after merging development with feature/message branch in client has been resolved

--- client/src/pages/HomePage/HomePage.jsx
+++ client/src/pages/HomePage/HomePage.jsx
@@ -1,30 +1,18 @@
 import styles from "./HomePage.module.css";
-<<<<<<< HEAD
 import PiratePxPageRender from "../../components/reusableComponents/PiratePxPageRender/PiratePxPageRender";
 import NavigateButton from "../../components/reusableComponents/NavigateButton/NavigateButton";
 import HelmetMetaTagsNetlify from "../../components/reusableComponents/HelmetMetaTagsNetlify/HelmetMetaTagsNetlify";
-=======
-import { Link } from "react-router-dom";
-import Message from "../../components/Message";
->>>>>>> feature/message
 
 const HomePage = () => {
   return (
     <main className={styles.container}>
-<<<<<<< HEAD
       {/* Track page renders */}
       <PiratePxPageRender COUNT_IDENTIFIER={"homepage"} />
       {/* Set page title and meta tags */}
-      <HelmetMetaTagsNetlify title='gatha - get together' />
+      <HelmetMetaTagsNetlify title="gatha - get together" />
       <h1>gatha - get together</h1>
       <p>This is the homepage.</p>
       <NavigateButton route={"user-registration"} buttonText={"Register"} />
-=======
-      {/*     <h1>gatha - get together</h1>
-      <p>This is the homepage.</p> */}
-      <Message />
-      {/*       <Link to="/user-registration">Register</Link> */}
->>>>>>> feature/message
     </main>
   );
 };
