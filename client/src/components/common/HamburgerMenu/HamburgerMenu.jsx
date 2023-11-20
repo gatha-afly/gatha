51b5cb0dcf3e32131c3a8926c19d20ce38140253
felixschmidt89 <felix.schmidt@dci.education>
2023-11-20 19:08:44 +0100

    added HamburgerMenu

---
+++ client/src/components/common/HamburgerMenu/HamburgerMenu.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import UsernameInitials from "../UsernameInitials/UsernameInitials";
+import styles from "./HamburgerMenu.module.css";
+
+/** * Responsive hamburger menu displaying the initials if a user as icon.
+ * @param {Object} props - The properties of the component.
+ * @param {string} props.firstName - The first name of the user for displaying initials.
+ * @param {string} props.lastName - The last name of the user for displaying initials.
+ * @returns {JSX.Element} React component.
+ */
+const HamburgerMenu = ({ firstName, lastName }) => {
+  // State to track the menu's open/closed status
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  // Toogle the menu's open/closed status
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  return (
+    <div className={styles.hamburgerMenu}>
+      <div
+        className={`${styles.menuIcon} ${menuOpen ? styles.open : ""}`}
+        onClick={toggleMenu}>
+        {/* Display user initials */}
+        <UsernameInitials
+          firstName={firstName}
+          lastName={lastName}
+          radius={6}
+          fontSize={2}
+        />
+      </div>
+
+      {menuOpen && (
+        <div className={styles.menuItems}>
+          <Link to='/user-logout'>Logout</Link>
+          <Link to='/user-logout'>Logout</Link>
+          <Link to='/user-logout'>Logout</Link>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default HamburgerMenu;
--- client/src/components/common/Header/Header.jsx
+++ client/src/components/common/Header/Header.jsx
@@ -1,27 +1,17 @@
 import { Link } from "react-router-dom";
 import styles from "./Header.module.css";
 import useUserContext from "../../../context/useUserContext";
-import UsernameInitials from "../UsernameInitials/UsernameInitials";
 import Logo from "../Logo/Logo";
+import HamburgerMenu from "../HamburgerMenu/HamburgerMenu";
 
 const Header = () => {
   const { loggedIn, user } = useUserContext();
   return (
     <header className={styles.header}>
       <Logo width={10} />
-      {/* Conditionally rendering the user initials info after successful login */}
-      {loggedIn ? (
-        <UsernameInitials
-          firstName={user.firstName}
-          lastName={user.lastName}
-          radius={5}
-          fontSize={2}
-        />
-      ) : null}
-
       {/* Conditionally render login and logout button */}
       {loggedIn ? (
-        <Link to='/user-logout'>Logout</Link>
+        <HamburgerMenu firstName={user.firstName} lastName={user.lastName} />
       ) : (
         <Link to='/user-login'>Login</Link>
       )}
