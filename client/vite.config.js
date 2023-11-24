0704d5b3f032efc3a27630abbd4e3ec97dadda66
felixschmidt89 <felix.schmidt@dci.education>
2023-11-24 12:24:47 +0100

    updated vite.config.js for netlify deployment

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,11 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ["@fortawesome/free-solid-svg-icons"],
+    },
+  },
   server: {
     port: 3000,
   },
