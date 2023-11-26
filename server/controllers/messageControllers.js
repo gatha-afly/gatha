58337b4db528f64657604bbba7e21f8232ce615a
Abdulwase Naeemi <abdulwasenaeemi7@gmail.com>
2023-11-26 01:47:35 +0100

    bug fixed

--- server/controllers/messageControllers.js
+++ server/controllers/messageControllers.js
@@ -1,40 +1,22 @@
-// messageControllers.js
 import Message from "../models/Message.js";
-import User from "../models/User.js";
 
-export const getInitialMessages = async (io, socket) => {
+export const getInitialMessages = async () => {
   try {
-    const messages = await Message.find({ room: { $in: socket.rooms } })
-      .sort({ createdAt: -1 })
-      .limit(10)
-      .populate("sender", "username");
-
-    io.to(socket.id).emit("init", messages.reverse()); // Emit to the specific socket
-  } catch (error) {
-    console.error(error);
+    const messages = await Message.find().sort({ createdAt: -1 }).limit(10);
+    return messages.reverse();
+  } catch (err) {
+    console.error(err);
+    throw err;
   }
 };
 
-export const sendMessage = async (io, text, senderId, room) => {
+export const sendMessage = async (text) => {
   try {
-    const senderExists = await User.exists({ _id: senderId });
-
-    if (!senderExists) {
-      console.error("Sender does not exist");
-      return;
-    }
-
-    const newMessage = new Message({
-      text: text,
-      sender: senderId,
-      room: room,
-    });
-
-    await newMessage.save();
-
-    await newMessage.populate("sender", "username").execPopulate();
-    io.to(room).emit("receive_message", newMessage);
-  } catch (error) {
-    console.error(error);
+    const message = new Message({ text });
+    await message.save();
+    return message;
+  } catch (err) {
+    console.error(err);
+    throw err;
   }
 };
--- server/routes/messageRoutes.js
+++ server/routes/messageRoutes.js
@@ -1,29 +1,28 @@
 import {
   getInitialMessages,
   sendMessage,
-} from "../controllers/messageControllers.js";
-import Message from "../models/Message.js";
+} from "../controllers/messageController.js";
 
 const setupSocketIO = (io) => {
   io.on("connection", async (socket) => {
     console.log(`User Connected: ${socket.id}`);
 
     try {
-      const messages = await Message.find().sort({ createdAt: -1 }).limit(10);
-      socket.emit("init", messages.reverse());
+      const messages = await getInitialMessages();
+      socket.emit("init", messages);
     } catch (err) {
       console.error(err);
     }
 
     socket.on("message", async (msg) => {
       try {
-        const message = new Message({ text: msg });
-        await message.save();
+        const message = await sendMessage(msg);
         io.emit("message", message);
       } catch (err) {
         console.error(err);
       }
     });
+
     socket.on("disconnect", () => {
       console.log("User disconnected");
     });
