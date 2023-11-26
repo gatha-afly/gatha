b6b69a896247b64841bfd291ee0e00dd6e45081a
Abdulwase Naeemi <abdulwasenaeemi7@gmail.com>
2023-11-26 01:51:30 +0100

    bug fixed

--- server/controllers/messageControllers.js
+++ server/controllers/messageControllers.js
@@ -1,21 +1,37 @@
 import Message from "../models/Message.js";
 
+/**
+ * Handler for getting the initial messages
+ * @returns
+ */
 export const getInitialMessages = async () => {
   try {
+    // Retrieve the latest 10 messages from the database
     const messages = await Message.find().sort({ createdAt: -1 }).limit(10);
+    // Reverse the order to have the oldest messages first
     return messages.reverse();
   } catch (err) {
+    // Log and throw any errors that occur during the operation
     console.error(err);
     throw err;
   }
 };
 
+/**
+ * Handler for sending a new message
+ * @param {*} text
+ * @returns
+ */
 export const sendMessage = async (text) => {
   try {
+    // Create a new message using the provided text
     const message = new Message({ text });
+    // Save the message to the database
     await message.save();
+    // Return the saved message
     return message;
   } catch (err) {
+    // Log and throw any errors that occur during the operation
     console.error(err);
     throw err;
   }
--- server/routes/messageRoutes.js
+++ server/routes/messageRoutes.js
@@ -3,30 +3,41 @@ import {
   sendMessage,
 } from "../controllers/messageController.js";
 
+// Function to set up Socket.IO
 const setupSocketIO = (io) => {
   io.on("connection", async (socket) => {
+    // Log the connection of a user
     console.log(`User Connected: ${socket.id}`);
 
     try {
+      // Get the initial messages and emit them to the connected client
       const messages = await getInitialMessages();
       socket.emit("init", messages);
     } catch (err) {
+      // Log any errors that occur during the operation
       console.error(err);
     }
 
+    // Listen for incoming messages from the client
     socket.on("message", async (msg) => {
       try {
+        // Send the received message to the messageController for processing
         const message = await sendMessage(msg);
+        // Broadcast the message to all connected clients
         io.emit("message", message);
       } catch (err) {
+        // Log any errors that occur during the operation
         console.error(err);
       }
     });
 
+    // Listen for disconnection events
     socket.on("disconnect", () => {
+      // Log when a user disconnects
       console.log("User disconnected");
     });
   });
 };
 
+// Export the setupSocketIO function
 export default setupSocketIO;
