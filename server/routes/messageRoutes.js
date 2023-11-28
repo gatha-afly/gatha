ffe9b042eb0543aa684e0e756ea2f1426129430c
Abdulwase Naeemi <abdulwasenaeemi7@gmail.com>
2023-11-28 15:43:19 +0100

    the auth middleware has been injected to the messageRouter endpoint

--- server/routes/messageRoutes.js
+++ server/routes/messageRoutes.js
@@ -1,8 +1,10 @@
 import express from "express";
 import { getAllGroupMessage } from "../controllers/messageControllers.js";
+import { authorizeUser } from "../middleware/express/userAuthorization.js";
 
 const router = express.Router();
 
+router.use(authorizeUser);
 router.get("/:groupId", getAllGroupMessage);
 
 export default router;
