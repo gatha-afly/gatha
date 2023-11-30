4a45c0e241f4e63b6b6a834d851314f316bf0cc4
besincielement <yesim.demir@dci.education>
2023-11-30 15:10:46 +0100

    Logo and icons added

--- client/src/components/features/GroupsList/GroupsManagementBar/GroupsManagementBar.jsx
+++ client/src/components/features/GroupsList/GroupsManagementBar/GroupsManagementBar.jsx
@@ -1,16 +1,20 @@
 // GroupBar.jsx
 import React from "react";
 import styles from "./GroupsManagementBar.module.css";
-import { FaPlusSquare } from "react-icons/fa";
-import { MdSearch } from "react-icons/md";
+import { GrGroup } from "react-icons/gr";
+import { AiOutlineUsergroupAdd } from "react-icons/ai";
 
 import ReactIconNavigate from "../../../common/ReactIconNavigate/ReactIconNavigate";
 
 function GroupsManagementBar() {
   return (
     <div className={styles.groupBar}>
-      <ReactIconNavigate route='/create-group' size={3} icon={FaPlusSquare} />
-      <ReactIconNavigate route='/join-group' size={3} icon={MdSearch} />
+      <ReactIconNavigate route="/create-group" size={3} icon={GrGroup} />
+      <ReactIconNavigate
+        route="/join-group"
+        size={3}
+        icon={AiOutlineUsergroupAdd}
+      />
     </div>
   );
 }
