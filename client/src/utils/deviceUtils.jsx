3f9c46de922e99333b789f4e7c662f43b9aefdd6
felixschmidt89 <felix.schmidt@dci.education>
2023-11-30 10:50:31 +0100

    Added new app logos with slogan to Logo component, refactored related Logo component

--- client/src/utils/deviceUtils.jsx
+++ client/src/utils/deviceUtils.jsx
@@ -1,5 +1,10 @@
 /**
  * Checks if the window width is less than or equal to 768 pixels to determine if it's a mobile device.
- * @returns {boolean} True if the window width is less than or equal to 768 pixels, indicating a mobile device.
+ * @returns {boolean}
  */
 export const isMobile = window.innerWidth <= 768;
+
+/**
+ * Checks if the window width is greater than or equal to 1025 pixels to determine if it's big screen device.
+ * @returns {boolean}
+ */ export const isBigScreen = window.innerWidth >= 1025;
