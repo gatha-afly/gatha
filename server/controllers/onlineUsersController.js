ddb657bbf2bc9bfef4b51bfdcac9c1819741fffc
Abdulwase Naeemi <abdulwasenaeemi7@gmail.com>
2023-12-03 09:39:52 +0100

    bug fixed

--- server/controllers/onlineUsersController.js
+++ server/controllers/onlineUsersController.js
@@ -8,19 +8,24 @@ import * as errorHandlerUtils from "../utils/errorHandler.js";
  * @param {*} res
  * @returns
  */
+
 export const getOnlineUser = async (req, res) => {
   try {
     const { userId } = req.params;
 
+    // Find the online user based on userId
     const onlineUser = await OnlineUsers.findOne({ userId });
 
     if (!onlineUser) {
-      return res
-        .status(StatusCodes.NOT_FOUND)
-        .json({ status: "offline", message: "User not found" });
+      return res.status(StatusCodes.NOT_FOUND).json({ status: "offline" });
     }
 
-    return res.status(StatusCodes.OK).json({ status: "online" });
+    // Return the online user object containing socketId
+    return res.status(StatusCodes.OK).json({
+      status: "online",
+      userId: onlineUser.userId,
+      socketId: onlineUser.socketId,
+    });
   } catch (error) {
     // Handle other internal errors using the utility function
     return errorHandlerUtils.handleInternalError(res);
