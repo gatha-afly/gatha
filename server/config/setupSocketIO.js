f747ce4b84bd8f4f494cc2c16d70c21a9f3a65f7
Abdulwase Naeemi <abdulwasenaeemi7@gmail.com>
2023-12-13 10:55:32 +0100

    bug fixed

--- server/config/setupSocketIO.js
+++ server/config/setupSocketIO.js
@@ -13,93 +13,100 @@ const setupSocketIO = (io) => {
   io.use(socketAuthMiddleware, isUserAlreadyGroupMember);
 
   io.on("connection", async (socket) => {
+    // Retrieve the user based on the socket user id
     const user = await User.findById(socket.user.id);
 
-    // On connection set the (is_online === true)
-    await User.findByIdAndUpdate(user._id, { is_online: true });
+    // Check if a user is found
+    if (user) {
+      // On connection set the (is_online === true)
+      await User.findByIdAndUpdate(user._id, { is_online: true });
 
-    // user joins rooms with groupId
-    user.groups.forEach((groupId) => {
-      socket.join(groupId.toString());
-    });
-
-    // Call getInitialMessages once after the user joins all groups
-    user.groups.forEach((groupId) => {
-      getInitialMessages(io, socket, groupId);
-      getUserStatus(io, socket, groupId);
-
-      // Notify other clients when a user is typing
-      socket.on("typing", ({ groupId }) => {
-        const userTyping = {
-          firstName: user.firstName,
-          lastName: user.lastName,
-          username: user.username,
-        };
-        socket
-          .to(groupId.toString())
-          .emit("typing", { user: userTyping, groupId: groupId });
-      });
-
-      // Notify other clients when a user stops typing
-      socket.on("stop_typing", ({ groupId }) => {
-        const userStoppedTyping = {
-          firstName: user.firstName,
-          lastName: user.lastName,
-          username: user.username,
-        };
-        socket
-          .to(groupId.toString())
-          .emit("stop_typing", { user: userStoppedTyping, groupId: groupId });
-      });
-    });
-
-    console.log(`User Connected: ${socket.id}`);
-
-    // Listen for incoming messages from the client
-    socket.on("send_message", async ({ text, groupId }, acknowledgment) => {
-      const userSendingMessage = {
-        firstName: user.firstName,
-        lastName: user.lastName,
-        username: user.username,
-      };
-
-      // Notify other clients when a user stops typing
-      socket
-        .to(groupId.toString())
-        .emit("stop_typing", { user: userSendingMessage });
-
-      // Send the received message to the messageController for processing
-      try {
-        // Socket.user.id is the id of the connected user
-        await sendMessage(io, text, socket.user.id, groupId);
-
-        // Send acknowledgment back to the client indicating success
-        acknowledgment({ success: true });
-      } catch (error) {
-        // Send acknowledgment back to the client with the error message
-        acknowledgment({ error: error.message });
-      }
-    });
-
-    // Listen for disconnection events
-    socket.on("disconnect", async () => {
-      // On disconnect set the (is_online === false)
-      await User.findByIdAndUpdate(user._id, { is_online: false });
-      console.log("User disconnected");
-
+      // user joins rooms with groupId
       user.groups.forEach((groupId) => {
+        socket.join(groupId.toString());
+      });
+
+      // Call getInitialMessages once after the user joins all groups
+      user.groups.forEach((groupId) => {
+        getInitialMessages(io, socket, groupId);
         getUserStatus(io, socket, groupId);
 
-        const userStoppedTyping = {
+        // Notify other clients when a user is typing
+        socket.on("typing", ({ groupId }) => {
+          const userTyping = {
+            firstName: user.firstName,
+            lastName: user.lastName,
+            username: user.username,
+          };
+          socket
+            .to(groupId.toString())
+            .emit("typing", { user: userTyping, groupId: groupId });
+        });
+
+        // Notify other clients when a user stops typing
+        socket.on("stop_typing", ({ groupId }) => {
+          const userStoppedTyping = {
+            firstName: user.firstName,
+            lastName: user.lastName,
+            username: user.username,
+          };
+          socket
+            .to(groupId.toString())
+            .emit("stop_typing", { user: userStoppedTyping, groupId: groupId });
+        });
+      });
+
+      console.log(`User Connected: ${socket.id}`);
+
+      // Listen for incoming messages from the client
+      socket.on("send_message", async ({ text, groupId }, acknowledgment) => {
+        const userSendingMessage = {
           firstName: user.firstName,
           lastName: user.lastName,
           username: user.username,
         };
+
+        // Notify other clients when a user stops typing
         socket
           .to(groupId.toString())
-          .emit("stop_typing", { user: userStoppedTyping, groupId: groupId });
+          .emit("stop_typing", { user: userSendingMessage });
+
+        // Send the received message to the messageController for processing
+        try {
+          // Socket.user.id is the id of the connected user
+          await sendMessage(io, text, socket.user.id, groupId);
+
+          // Send acknowledgment back to the client indicating success
+          acknowledgment({ success: true });
+        } catch (error) {
+          // Send acknowledgment back to the client with the error message
+          acknowledgment({ error: error.message });
+        }
       });
-    });
+
+      // Listen for disconnection events
+      socket.on("disconnect", async () => {
+        // On disconnect set the (is_online === false)
+        await User.findByIdAndUpdate(user._id, { is_online: false });
+        console.log("User disconnected");
+
+        user.groups.forEach((groupId) => {
+          getUserStatus(io, socket, groupId);
+
+          const userStoppedTyping = {
+            firstName: user.firstName,
+            lastName: user.lastName,
+            username: user.username,
+          };
+          socket
+            .to(groupId.toString())
+            .emit("stop_typing", { user: userStoppedTyping, groupId: groupId });
+        });
+      });
+    } else {
+      // Handle the case where the user is not found
+      console.log("User not found");
+    }
   });
 };
 
